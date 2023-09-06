8e49b20f419c4021564bf7441cf9ba305a77665e
Artem Bolotov <teeemon@gmail.com>
2023-09-06 07:29:27 +0300

    Initial Commit

---
+++ HelloRedux/HelloReduxApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloReduxApp.swift
+//  HelloRedux
+//
+//  Created by artembolotov on 06.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct HelloReduxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
